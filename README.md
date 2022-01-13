2b4481377dada96d8cd0ea21abd38c5576840276
Your Name <you@example.com>
2022-01-13 09:59:43 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tips and tricks
+
+ng add @ngrx/store
+
+create store for features
+
+    StoreModule.forFeature('products',{})
---
+++ apm/src/app/products/state/product.reducer.ts
@@ -0,0 +1,13 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+
+export const productReducer=createReducer(
+  { showProductCode: true },
+  on(createAction('[Product] Toggle Product Code'), state => {
+    console.log('originate state: ' + JSON.stringify(state.showProductCode))
+    return {
+      ...state,
+      showProductCode: !state.showProductCode
+      // showProductCode: state.showProductCode = false,
+    };
+  })
+);
